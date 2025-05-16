1d9ed161bd673268305f25112245b99ce1df3f61
Mazen Saad <mmes3666@gmail.com>
2025-05-16 18:25:24 +0200

    feat(model): remove omitempty from description fields to make them required

--- internal/model/types.go
+++ internal/model/types.go
@@ -3,12 +3,12 @@ package model
 type Link struct {
 	Title       string `yaml:"title"`   
 	URL         string `yaml:"url"`
-	Description string `yaml:"description,omitempty"`
+	Description string `yaml:"description"`
 }
 
 type Category struct {
 	Title         string     `yaml:"title"`
-	Description   string     `yaml:"description,omitempty"`
+	Description   string     `yaml:"description"`
 	Links         []Link     `yaml:"links,omitempty"`
 	Subcategories []Category `yaml:"subcategories,omitempty"`
 
