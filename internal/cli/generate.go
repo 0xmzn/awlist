db3a6fdd2b933751ca7a9ea20c14e4fe63b54750
Mazen Saad <mmes3666@gmail.com>
2025-05-16 21:12:34 +0200

    refactor(cli): extract responsibilities from GenerateCmd.Run into helper functions

--- internal/cli/generate.go
+++ internal/cli/generate.go
@@ -18,57 +18,87 @@ type GenerateCmd struct {
 	Html       bool     `kong:"optional,name='html',help='Generate HTML output.'"`
 }
 
+func loadAndEnrichData(dataFile string) (*model.AwesomeData, error) {
+	dataPath, err := GetDataFilePath(dataFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get data file path: %w", err)
+	}
+
+	store, err := awesomestore.NewStore(dataPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load data from %s: %w", dataPath, err)
+	}
+
+	awesomeData := store.Data()
+	model.Enrich(&awesomeData)
+	return &awesomeData, nil
+}
+
+func getOutputWriter(outputFilePath string) (writer io.Writer, closer func() error, err error) {
+	if outputFilePath == "" {
+		return os.Stdout, nil, nil
+	}
+	file, errCreate := os.Create(outputFilePath)
+	if errCreate != nil {
+		return nil, nil, fmt.Errorf("failed to create output file %s: %w", outputFilePath, errCreate)
+	}
+	return file, file.Close, nil
+}
+
+func executeHTMLTemplate(writer io.Writer, templateContent string, data *model.AwesomeData) error {
+	tmpl := htmltemplate.New("user-template-html").Option("missingkey=error")
+	parsedTmpl, err := tmpl.Parse(templateContent)
+	if err != nil {
+		return fmt.Errorf("html template parsing error: %w", err)
+	}
+	err = parsedTmpl.Execute(writer, data)
+	if err != nil {
+		return fmt.Errorf("failed to execute html template: %w", err)
+	}
+	return nil
+}
+
+func executeTextTemplate(writer io.Writer, templateContent string, data *model.AwesomeData) error {
+	tmpl := texttemplate.New("user-template-text").Option("missingkey=error")
+	parsedTmpl, err := tmpl.Parse(templateContent)
+	if err != nil {
+		return fmt.Errorf("text template parsing error: %w", err)
+	}
+	err = parsedTmpl.Execute(writer, data)
+	if err != nil {
+		return fmt.Errorf("failed to execute text template: %w", err)
+	}
+	return nil
+}
+
 func (cmd *GenerateCmd) Run() error {
 	inputFileContent, err := os.ReadFile(cmd.InputFile)
 	if err != nil {
 		return fmt.Errorf("failed to read input file: %w", err)
 	}
 
-	dataPath, err := GetDataFilePath(cmd.Globals.DataFile)
+	awesomeData, err := loadAndEnrichData(cmd.Globals.DataFile)
 	if err != nil {
-		return fmt.Errorf("failed to get data file path: %w", err)
+		return err
 	}
 
-	store, err := awesomestore.NewStore(dataPath)
+	outputWriter, closeFile, err := getOutputWriter(cmd.OutputFile)
 	if err != nil {
-		return fmt.Errorf("failed to load data from %s: %w", dataPath, err)
+		return err
+	}
+	if closeFile != nil {
+		defer closeFile()
 	}
 
-	awesomeData := store.Data()
-	model.Enrich(&awesomeData)
-
-	var outputWriter io.Writer
-	if cmd.OutputFile == "" {
-		outputWriter = os.Stdout
-	} else {
-		file, err := os.Create(cmd.OutputFile)
-		if err != nil {
-			return fmt.Errorf("failed to create output file %s: %w", cmd.OutputFile, err)
-		}
-		defer file.Close()
-		outputWriter = file
-	}
-
+	templateStr := string(inputFileContent)
 	if cmd.Html {
-		tmpl := htmltemplate.New("user-template-html").Option("missingkey=error")
-		tmpl, err = tmpl.Parse(string(inputFileContent))
-		if err != nil {
-			return fmt.Errorf("html template parsing error: %w", err)
-		}
-		err = tmpl.Execute(outputWriter, awesomeData)
-		if err != nil {
-			return fmt.Errorf("failed to execute html template: %w", err)
-		}
+		err = executeHTMLTemplate(outputWriter, templateStr, awesomeData)
 	} else {
-		tmpl := texttemplate.New("user-template-text").Option("missingkey=error")
-		tmpl, err = tmpl.Parse(string(inputFileContent))
-		if err != nil {
-			return fmt.Errorf("text template parsing error: %w", err)
-		}
-		err = tmpl.Execute(outputWriter, awesomeData)
-		if err != nil {
-			return fmt.Errorf("failed to execute text template: %w", err)
-		}
+		err = executeTextTemplate(outputWriter, templateStr, awesomeData)
+	}
+
+	if err != nil {
+		return err
 	}
 
 	return nil
